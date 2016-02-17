1fe7ddb873b951f9d935bf440dd4ce6fb9435e95
Antonio Pagano <apagano@wawand.co>
2016-02-17 10:13:21 -0500

    [config] adding compose and count routes

--- config/routes.go
+++ config/routes.go
@@ -8,6 +8,7 @@ import (
 //defineRoutes gets called by the generated main.go, please define routes inside it.
 func DefineRoutes(r *routing.Router) {
 	r.Get("/", handlers.Hello)
+	r.Get("/count", handlers.Count)
 	r.Get("/hello", handlers.Hello)
 	r.Get("/who_are_you", handlers.ContainerInfo)
 }
---
+++ handlers/counter.go
@@ -0,0 +1,30 @@
+package handlers
+
+import (
+	"log"
+	"net/http"
+	"strconv"
+	"time"
+
+	"gopkg.in/mgo.v2"
+)
+
+type Visit struct {
+	Date time.Time
+}
+
+func Count(rw http.ResponseWriter, req *http.Request) {
+	session, err := mgo.Dial("mongo")
+	defer session.Close()
+
+	if err != nil {
+		log.Println("Error Connecting:" + err.Error())
+	}
+
+	c := session.DB("default").C("visits")
+	_ = c.Insert(Visit{Date: time.Now()})
+
+	var count int
+	count, err = c.Count()
+	rw.Write([]byte("Visits Count: " + strconv.Itoa(count)))
+}
